254b0e412d4897f7ae0fd4181b4615dddf0f0f07
mhj9030 <mhj9030@gmail.com>
2017-06-19 19:25:34 +0900

    로그인 페이지 디자인 일부 완성

---
+++ final_project/src/main/java/com/final_project/main/MainController.java
@@ -0,0 +1,16 @@
+package com.final_project.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("mainController")
+public class MainController {
+	
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String method() {
+		return ".mainLayout";
+
+	}
+
+}
