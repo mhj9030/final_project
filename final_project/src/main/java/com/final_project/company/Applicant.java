fe6d2823abbbe1114504f0685741dbf89d979625
user1 <user1@user1-PC>
2017-07-11 09:46:47 +0900

    1

---
+++ final_project/src/main/java/com/final_project/company/Applicant.java
@@ -0,0 +1,58 @@
+package com.final_project.company;
+
+public class Applicant {
+	private String mId;
+	private int rNum, ceNum;
+	private String appDate;
+	private String mName, mGender;
+	private String proPhoto, proIntro;
+	
+	public String getmId() {
+		return mId;
+	}
+	public void setmId(String mId) {
+		this.mId = mId;
+	}
+	public int getrNum() {
+		return rNum;
+	}
+	public void setrNum(int rNum) {
+		this.rNum = rNum;
+	}
+	public int getCeNum() {
+		return ceNum;
+	}
+	public void setCeNum(int ceNum) {
+		this.ceNum = ceNum;
+	}
+	public String getAppDate() {
+		return appDate;
+	}
+	public void setAppDate(String appDate) {
+		this.appDate = appDate;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public String getmGender() {
+		return mGender;
+	}
+	public void setmGender(String mGender) {
+		this.mGender = mGender;
+	}
+	public String getProPhoto() {
+		return proPhoto;
+	}
+	public void setProPhoto(String proPhoto) {
+		this.proPhoto = proPhoto;
+	}
+	public String getProIntro() {
+		return proIntro;
+	}
+	public void setProIntro(String proIntro) {
+		this.proIntro = proIntro;
+	}
+}
---
+++ final_project/src/main/java/com/final_project/company/ApplicantService.java
@@ -0,0 +1,8 @@
+package com.final_project.company;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ApplicantService {
+	public List<Applicant> listApplicant(Map<String, Object> map) throws Exception;
+}
