a24e6fc7ccb0e33947f53128e7d270673385c53d
user1 <user1@user1-PC>
2017-07-28 13:43:18 +0900

    1

--- final_project/src/main/java/com/final_project/submain/SubmainController.java
+++ final_project/src/main/java/com/final_project/submain/SubmainController.java
@@ -1,6 +1,7 @@
 package com.final_project.submain;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,7 +9,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class SubmainController {
 	
 	@RequestMapping(value = "/submain", method = RequestMethod.GET)
-	public String method() {
+	public String method(
+			String login_error,
+			Model model
+			) {
+		
+		if(login_error!=null)
+			model.addAttribute("mode", "loginfail");
+		
 		return ".subLayout";
 
 	}
